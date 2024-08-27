468768c6f818599b97d8277794bcf31f0901afba
侯国玉 <93714738+KenCao2007@users.noreply.github.com>
2024-08-26 19:53:37 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -6,18 +6,14 @@ import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
-import transformer.Constants as Constants
-from Utils import thp_loglikelihood, eval_accuracy # sahp_score_loglikelihood
+from Utils import eval_loglikelihood, eval_accuracy 
 import matplotlib.pyplot as plt
 from preprocess.Dataset import get_dataloader
-from transformer.Models import thp_Transformer, ithp_Transformer, get_non_pad_mask
+from transformer.THP import thp_Transformer, get_non_pad_mask
 from tqdm import tqdm
-import time
 from transformer.SAHP import SAHP
 import Utils
 import os
-import pickle
 eval_after = 10
 eval_gap = 10
 
@@ -52,8 +48,6 @@ def train_epoch(model, training_data, optimizer, pred_loss_func, opt):
     model.train()
     
     total_loss = 0
-    if opt.debug:
-        total_loss_gt = 0
     total_preds = 0
     idx = 0
     for batch in tqdm(training_data, mininterval=2,
@@ -86,7 +80,7 @@ def train_epoch(model, training_data, optimizer, pred_loss_func, opt):
 def train(model, training_data, validation_data, optimizer, scheduler, pred_loss_func, opt):
     """ Start training. """
     opt.train_able=False
-    best_tll = thp_loglikelihood(model, validation_data, opt)
+    best_tll = eval_loglikelihood(model, validation_data, opt)
     logging.info('Initial testing LogLikelihood: {}'.format(best_tll))
     torch.save(model.state_dict(), opt.results_saved_path + opt.model_saved_name + '.pth')
     logging.info("saving models to {}".format(opt.results_saved_path + opt.model_saved_name + '.pth'))
@@ -109,7 +103,7 @@ def train(model, training_data, validation_data, optimizer, scheduler, pred_loss
 
         """ evaluate the model """
         if epoch_i >= eval_after and epoch_i % eval_gap == 0:
-            ll_func = thp_loglikelihood
+            ll_func = eval_loglikelihood
             opt.train_able=False
             with torch.no_grad():  
                 test_ll = ll_func(model, validation_data, opt)
@@ -126,10 +120,6 @@ def train(model, training_data, validation_data, optimizer, scheduler, pred_loss
         torch.save(model.state_dict(), opt.results_saved_path + opt.model_saved_name + '.pth')
         logging.info("saving models to {}".format(opt.results_saved_path + opt.model_saved_name + '.pth'))
 
-        # if epoch_i % 10 == 0:
-        #     # torch.save(model.state_dict(), opt.model_saved_path + opt.model_saved_name + '.pth')
-        #     torch.save(model.state_dict(), opt.results_saved_path + opt.model_saved_name + '.pth') # save a copy to results folder
-        #     logging.info("saving models to {}".format(opt.results_saved_path + opt.model_saved_name + '.pth'))
 
     end_time = time.time()
     total_time = end_time - start_time
@@ -153,7 +143,7 @@ def main():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-data', type=str, default="data/retweet/")
+    parser.add_argument('-data', type=str, default="data/stackoverflow/")
 
     parser.add_argument('-epoch', type=int, default=100)
     parser.add_argument('-batch_size', type=int, default=64)
@@ -173,26 +163,21 @@ def main():
     parser.add_argument('-log', type=str, default='log.txt')
 
     parser.add_argument('-mode', type=str, default='intensity') # score matching mode: build by intensity or model score directly
-    parser.add_argument('-method', type=str, choices=["wsm","mle","dsm"], default='mle')
+    parser.add_argument('-method', type=str, choices=["wsm","mle","dsm"], default='wsm')
     parser.add_argument('-T_per_batch', type=bool, default=True) # T are set to be the maximum event time at each batch.
-    parser.add_argument('-model', type=str, choices=['thp_transformer', "ithp_transformer", "sahp"], default='sahp')
+    parser.add_argument('-model', type=str, choices=['thp_transformer', "ithp_transformer", "sahp"], default='thp_transformer')
     parser.add_argument('-train_able', type=int, default=1)
     parser.add_argument('-load_model', type=int, default=0)
     parser.add_argument('-num_grid', type=int, default=1)
     parser.add_argument('-CE_coef', type=float, default=1.0)
-    # parser.add_argument('-h_type', type=str, default="two_side_op", choices=['None','two_side_op','two_side_ord','one_side'])
     parser.add_argument('-h_type', type=str, default="two_side_op", choices=['None','two_side_op','two_side_ord','one_side_ord','one_side_opt'])
     parser.add_argument('-noise_var', type=float, default=1.0)
     parser.add_argument('-num_noise', type=int, default=1)
-    parser.add_argument('-seq_trunc', type=int, default=1)
+    parser.add_argument('-seq_trunc', type=int, default=0)
     parser.add_argument('-delete_outlier', type=int, default=0)
     parser.add_argument('-inconsistent_T', type=int, default=0)
-    parser.add_argument('-debug', type=bool, default=False)
     parser.add_argument('-seed', type=int, default=0)
-
-    parser.add_argument('-eval_per_epoch', type=int, default=0) # calculate log-likelihood every epoch. Memory consuming!
    
-
 
     opt = parser.parse_args()
     # default device is CUDA
@@ -213,7 +198,6 @@ def main():
         opt.model_saved_name = f'/{opt.model}_{opt.method}_{opt.data_name}_{opt.h_type}_alpha{opt.CE_coef}_epoch{opt.epoch}_{opt.seed}'
     elif opt.method == "dsm":
         opt.model_saved_name = f'/{opt.model}_{opt.method}_{opt.data_name}_alpha{opt.CE_coef}_noise{opt.noise_var}_num{opt.num_noise}_epoch{opt.epoch}_{opt.seed}'
-    # opt.model_saved_path = 'saved_model'
     opt.results_saved_path = 'results{dir_name}'.format(dir_name=opt.model_saved_name) # IMPORTANT: save model, logging, plots here
     if not os.path.exists(opt.results_saved_path):
         os.makedirs(opt.results_saved_path)
@@ -247,18 +231,6 @@ def main():
         )
         for param in model.parameters():
             param.data = param.data.double()
-    elif opt.model == "ithp_transformer":
-        model = ithp_Transformer(
-            num_types=num_types,
-            d_model=opt.d_model,
-            d_inner=opt.d_inner_hid,
-            n_layers=opt.n_layers,
-            n_head=opt.n_head,
-            d_k=opt.d_k,
-            d_v=opt.d_v,
-            dropout=opt.dropout,
-            opt = opt
-        )
     elif opt.model == "sahp":
         model = SAHP(model_config=opt)
         for param in model.parameters():
@@ -290,12 +262,11 @@ def main():
         train(model, trainloader, testloader, optimizer, scheduler, pred_loss_func, opt)
     
 
-    if not opt.eval_per_epoch:
-        opt.train_able=False
-        model.load_state_dict(torch.load(opt.results_saved_path + opt.model_saved_name + '.pth'))
-        tll = Utils.thp_loglikelihood(model, testloader, opt)
-        acc = Utils.eval_accuracy(model, testloader, opt)
-        logging.info("The testing tll and accuracy are {} and {}".format(tll, acc))
+    opt.train_able=False
+    model.load_state_dict(torch.load(opt.results_saved_path + opt.model_saved_name + '.pth'))
+    tll = Utils.eval_loglikelihood(model, testloader, opt)
+    acc = Utils.eval_accuracy(model, testloader, opt)
+    logging.info("The testing tll and accuracy are {} and {}".format(tll, acc))
 
     
 
